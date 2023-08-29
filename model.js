c6e6f8abec34f3c9924663b0778d87300fb0f5f9
Ariel Broukman <102252677+obeezy2@users.noreply.github.com>
2023-08-29 18:31:45 +0300

    last

--- model.js
+++ model.js
@@ -1,27 +1,43 @@
-const APIKEY = '2274382c58b50619bfc656928b96c3b9';
+// Define API key and URL
+const APIKEY = 'fdf42c920ed60eb207c0f7cc84f5dcbb';
 const URL = `http://api.exchangeratesapi.io/v1/latest?access_key=${APIKEY}`;
 
+// Initialize fetch counter
 let count = 0;
 
+// Asynchronous function to fetch exchange rate data
 async function fetchData() {
     try {
+        // Make the API call
         const response = await fetch(URL);
+
+        // Parse the JSON response
         const data = await response.json();
 
+        // Check if the API call was successful
         if (response.ok) {
+            // Pass the data to the displayData function
             displayData(data);
+
+            // Increment the fetch counter
             count++;
         } else {
+            // Log an error if the API call failed
             console.error("Error fetching data:", data.error);
         }
 
+        // Stop fetching after 24 successful calls
         if (count >= 24) {
             clearInterval(intervalId);
         }
     } catch (error) {
+        // Log any fetch errors
         console.error('Fetch error:', error);
     }
 }
 
-const intervalId = setInterval(fetchData, 10000);  // 3600000 ms = 1 hour
-fetchData();  // Initial fetch
+// Set an interval to fetch data every 360000 ms (1 hour)
+const intervalId = setInterval(fetchData, 360000);
+
+// Make an initial fetch when the script runs
+fetchData();
--- viewLogic.js
+++ viewLogic.js
@@ -1,33 +1,47 @@
+// Function to display exchange rate data
 function displayData(data) {
+    // Destructure the data into its components
     const { base, rates, date, timestamp } = data;
 
+    // Check for missing or invalid data
     if (!base || !rates || rates["ILS"] === undefined) {
         console.error("No data available for display.");
         return;
     }
 
+    // Convert the timestamp to a JavaScript Date object
     const localDate = new Date(timestamp * 1000);
+
+    // Get the time zone of the local system
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    // Format the time
     const formattedTime = formatTime(localDate);
 
+    // Append a new row to the table
     appendRowToTable(base, rates["ILS"], date, formattedTime);
 }
 
+// Function to format a JavaScript Date object as a time string
 function formatTime(date) {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
 }
 
+// Function to append a new row to the exchange rate table
 function appendRowToTable(base, rate, date, time) {
     const tableBody = document.getElementById("exchangeEuro");
 
     const row = document.createElement('tr');
+
+    // Create and append cells for currency, rate, time, and date
     ["td", "td", "td", "td"].forEach((_, index) => {
         const cell = document.createElement('td');
         cell.innerText = [base, rate, time, date][index];
         row.appendChild(cell);
     });
 
+    // Append the completed row to the table
     tableBody.appendChild(row);
 }
